05da098df42f99e5a6592d08380158d0e5b6b153
Richard Hall <hallboyone@icloud.com>
2022-02-21 20:36:32 -0500

    Have a bit-banging solution for the lmt01 sensor. Trying to get the pio solution to work.

--- src/main.c
+++ src/main.c
@@ -3,9 +3,8 @@
 #include "hardware/gpio.h"
 
 #include "components/uart.h"
-#include "components/max31855.h"
-#include "hx711.pio.h"
-#include "components/phasecontrol.h"
+//#include "hx711.pio.h"
+//#include "components/phasecontrol.h"
 #include "lmt01.pio.h"
 
 void packData(uint64_t scale_val,
@@ -13,12 +12,13 @@ void packData(uint64_t scale_val,
 	      uint64_t switch_vals,
 	      uint64_t * buf){
   *buf = 0;
+  // The bytes of the thermo value are reversed
+  *buf = ((thermo_val & 0xff000000)>>24) | ((thermo_val & 0x00ff0000)>>8) |
+         ((thermo_val & 0x0000ff00)<<8) | ((thermo_val & 0x000000ff)<<24); 
   //scale_val = 3;
   //scale_val <<= 30;
-  scale_val = 0;
-  //thermo_val = 0;
-  switch_vals = 0;
-  *buf = (((uint64_t)switch_vals)<<(24+14)) | (((uint64_t)thermo_val)) | ((uint64_t)(scale_val));
+  //thermo_val = 0; 
+  //*buf = (((uint64_t)switch_vals)<<(24+14)) | (((uint64_t)thermo_val)) | ((uint64_t)(scale_val));
 }
 
 int main(){
@@ -27,33 +27,29 @@ int main(){
   gpio_init(LED_PIN);
   gpio_set_dir(LED_PIN, GPIO_OUT);
 
+  /*
   // ========= Set up the scale ==========
   HX711 scale;
   const uint pio_num = 0;
   const uint dat_pin = 16;
   const uint clk_pin = 17;
   hx711_setup(&scale, pio_num, dat_pin, clk_pin);
-
-  /*
-  // ======== Set up thermocouple ========
-  MAX31855 thermo = {.spi = spi1,
-		     .cs  = 11,
-		     .clk = 10,
-		     .din = 12};
-  max31855_setup(&thermo);
   */
   // ======== Set up digital thermo ========
-  LMT01 thermo = {.pio_num = 0,
-		  .dat_pin = 12};
-  lmt01_setup(&thermo);
-
+  /*LMT01 thermo = {.pio_num = 1,
+                  .sig_pin = 15};
+		  lmt01_setup(&thermo);*/
+  gpio_init(15);
+  gpio_set_dir(15, GPIO_IN);
+  
+  /*
   // ======= Set up phase constrol =======
   PHASECONTROL_CONFIG pump_config = {.trigger         = RISING,
 				     .zerocross_pin   = 15,
 				     .out_pin         = 14,
 				     .zerocross_delay = 1620};
-  phasecontrol_setup(&pump_config);
-  
+vv  phasecontrol_setup(&pump_config);
+  */
   // ========== Set up the UART ==========
   UART pi_uart = {.id = uart1,
 		  .tx_pin = 4,
@@ -62,10 +58,10 @@ int main(){
   uart_setup(&pi_uart);
 
   
-  uint8_t msg_buf[4];
+  //uint8_t msg_buf[4];
   uint64_t payload = 0;
-  bool led_state = false;
-  int dir = 1;
+  //bool led_state = false;
+  //int dir = 1;
   while(1){
     //if(uart_data_in_rx(&pi_uart)){
       //uart_read_and_clear(&pi_uart, msg_buf, 4);
@@ -74,15 +70,40 @@ int main(){
       //uart_send(&pi_uart, (uint8_t*)&scale_val, 4);
       
     //}
-
-    // Read sensors
-    hx711_read(&scale);
-    lmt01_read(&thermo);
-    packData(scale.val, (uint32_t)thermo.val, 0, &payload);
-    uart_send(&pi_uart, (uint8_t*)&thermo.val, 4);
     
-    sleep_ms(1000);
-    gpio_put(LED_PIN, led_state);
-    led_state = !led_state;
+    uint32_t counter = 0;
+    // Go high for at least 25ms
+    absolute_time_t sample_time = get_absolute_time();
+    while(absolute_time_diff_us(sample_time, get_absolute_time()) < 25000){
+      if(!gpio_get(15)){
+	while(!gpio_get(15)){
+	  tight_loop_contents();
+	}
+	sample_time = get_absolute_time();
+      }
+    }
+    while(gpio_get(15)){
+      tight_loop_contents();
+    }
+    sample_time = get_absolute_time();
+    // Count pulses till signal high for 25ms
+    while(absolute_time_diff_us(sample_time, get_absolute_time()) < 25000){
+      if(!gpio_get(15)){
+	counter += 1;
+	while(!gpio_get(15)){
+	  tight_loop_contents();
+	}
+	sample_time = get_absolute_time();
+      }
+    }
+    // Read sensors
+    //hx711_read(&scale);
+    //lmt01_read(&thermo);
+    packData(0, counter, 0, &payload);
+    uart_send(&pi_uart, (uint8_t*)&payload, 8);
+    //uart_send(&pi_uart, (uint8_t*)&counter, 1);
+    //sleep_ms(50);
+    gpio_put(LED_PIN, gpio_get(15));
+    //led_state = !led_state;
   }
 }
