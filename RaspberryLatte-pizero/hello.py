707176d416f286efb790aa7886c5d6df7b9dabed
Richard Hall <hallboyone@icloud.com>
2022-04-20 14:48:18 -0400

    Sorted out restructuring issues.

---
+++ RaspberryLatte-pizero/hello.py
@@ -0,0 +1,24 @@
+import serial
+import bitstruct
+from time import sleep
+
+def getPressure():
+    msg_get_pressure = bitstruct.pack('u4u4', 5, 0)
+    ser.write(msg_get_pressure)
+    while(ser.in_waiting==0):
+        pass
+    sleep(0.01)
+    response = bitstruct.unpack('u4u4u16', ser.read_all())
+    print(f"Current pressure: {response[2]}")
+    #print(bitstruct.unpack('u4u4u16', response))
+
+
+ser = serial.Serial(port="/dev/ttyS0", baudrate = 115200)
+getPressure()
+#while(True):
+    # msg_id = int(input("Enter message ID\n> "))
+    # msg_len = 1
+    # msg_body = int(input("Enter message body value\n> "))
+    # msg = bitstruct.pack('u4u4u8', msg_id, msg_len, msg_body)
+    # print(msg)
+    # ser.write(msg)
