57dbff7601c375dfe6b351f67fa537f49a774802
Richard Hall <hallboyone@icloud.com>
2022-04-27 22:05:42 -0400

    Moved pinout to header file.

---
+++ RaspberryLatte-pico/include/pinout.h
@@ -0,0 +1,16 @@
+#define PRESSURE_SENSOR_PIN        28
+
+#define PUMP_SWITCH_PIN            16
+#define DIAL_0_PIN                 17
+#define DIAL_1_PIN                 18
+#define DIAL_2_PIN                 19
+#define DIAL_3_PIN                 20
+
+#define PHASE_CONTROL_0CROSS_PIN   15
+#define PHASE_CONTROL_OUT_PIN      14
+#define PHASE_CONTROL_0CROSS_SHIFT 300
+
+#define SCALE_DATA_PIN             3
+#define SCALE_CLOCK_PIN            2
+
+#define HEATER_PWM_PIN             0
--- RaspberryLatte-pico/src/main.c
+++ RaspberryLatte-pico/src/main.c
@@ -1,6 +1,8 @@
 #include "pico/stdlib.h"
 #include "pico/time.h"
 
+#include "pinout.h"
+
 #include "maintainer.h"
 #include "phasecontrol.h"
 #include "binary_input.h"
@@ -30,27 +32,26 @@ int main(){
 
   gpio_init(PICO_DEFAULT_LED_PIN);
   gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-  assignHandler(MSG_ID_END_PROGRAM, &endProgram);
 
-  pressure_sensor_setup(28);
+  pressure_sensor_setup(PRESSURE_SENSOR_PIN);
 
-  const uint8_t pump_switch_gpio = 16;
-  const uint8_t mode_select_gpio[4] = {17,18,19,20};
+  const uint8_t pump_switch_gpio = PUMP_SWITCH_PIN;
+  const uint8_t mode_select_gpio[4] = {DIAL_0_PIN, DIAL_1_PIN, DIAL_2_PIN, DIAL_3_PIN};
   binary_input_setup(1, &pump_switch_gpio, true);
   binary_input_setup(4, mode_select_gpio, true);
   
   // Set up phase control
   PhasecontrolConfig pump_config = 
   {.event           = FALLING,
-  .zerocross_pin   = 15,
-  .out_pin         = 14,
-  .zerocross_shift = 300};
+  .zerocross_pin   = PHASE_CONTROL_0CROSS_PIN,
+  .out_pin         = PHASE_CONTROL_OUT_PIN,
+  .zerocross_shift = PHASE_CONTROL_0CROSS_SHIFT};
   phasecontrol_setup(&pump_config);
 
-  hx711_setup(0, 3, 2);
+  hx711_setup(0, SCALE_DATA_PIN, SCALE_CLOCK_PIN);
 
-  heater_setup(PICO_DEFAULT_LED_PIN);
-  
+  heater_setup(HEATER_PWM_PIN);
+
   // Continually look for a message and then run maintenance
   while(run){
     readMessage();
