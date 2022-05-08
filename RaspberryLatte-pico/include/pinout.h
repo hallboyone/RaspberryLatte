c21f2a9da562c19bb50b66e0838e854342e0a179
Richard Hall <hallboyone@icloud.com>
2022-05-08 00:07:49 -0400

    Updated pinout

--- RaspberryLatte-pico/include/pinout.h
+++ RaspberryLatte-pico/include/pinout.h
@@ -1,8 +1,8 @@
 #define PRESSURE_SENSOR_PIN        28
 
-#define PUMP_SWITCH_PIN            16
-#define DIAL_A_PIN                 17
-#define DIAL_B_PIN                 18
+#define PUMP_SWITCH_PIN            9
+#define DIAL_A_PIN                 7
+#define DIAL_B_PIN                 8
 
 #define PHASE_CONTROL_0CROSS_PIN   4
 #define PHASE_CONTROL_OUT_PIN      8
@@ -11,7 +11,7 @@
 #define SCALE_DATA_PIN             3
 #define SCALE_CLOCK_PIN            2
 
-#define HEATER_PWM_PIN             7
+#define HEATER_PWM_PIN             10
 
 #define LMT01_DATA_PIN             6
 
