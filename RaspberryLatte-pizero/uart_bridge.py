cf1497ad7ae14d9746bce03ba7af96d3988c5c37
Richard Hall <rah75@duke.edu>
2022-05-23 23:36:50 -0400

    Removed parts of bridge moved to components. Still need to rework setters.

--- RaspberryLatte-pizero/uart_bridge.py
+++ RaspberryLatte-pizero/uart_bridge.py
@@ -1,14 +1,4 @@
 import serial
-import bitstruct
-import sys
-from time import sleep, time
-
-ser = serial.Serial(port="/dev/ttyS0", baudrate = 115200)
-
-_MSG_ID_SET_LEDS      =  1
-_MSG_ID_SET_PUMP      =  2
-_MSG_ID_SET_SOLENOID  =  3
-_MSG_ID_SET_HEATER    =  4
 
 _MSG_ID_GET_SWITCH    =  8
 _MSG_ID_GET_PRESSURE  =  9
@@ -16,23 +6,36 @@ _MSG_ID_GET_WEIGHT    = 10
 _MSG_ID_GET_TEMP      = 11
 _MSG_ID_GET_AC_ON     = 12
 
-_MSG_LEN_GET_SWITCH   =  3
-_MSG_LEN_GET_PRESSURE =  3
-_MSG_LEN_GET_WEIGHT   =  4
-_MSG_LEN_GET_TEMP     =  3
-_MSG_LEN_GET_AC_ON    =  2
+_SERIAL_PORT = "/dev/ttyS0"
+_BAUDRATE = 115200
 
-_get_pressure_msg = bitstruct.pack('u4u4', _MSG_ID_GET_PRESSURE, 0)
-_get_temp_msg     = bitstruct.pack('u4u4', _MSG_ID_GET_TEMP, 0)
-_get_switch_msg   = bitstruct.pack('u4u4', _MSG_ID_GET_SWITCH, 0)
-_get_weight_msg   = bitstruct.pack('u4u4', _MSG_ID_GET_WEIGHT, 0)
-_get_ac_on_msg    = bitstruct.pack('u4u4', _MSG_ID_GET_AC_ON, 0)
+_ser = serial.Serial(port=_SERIAL_PORT, baudrate = _BAUDRATE)
+_header_decoder = bitstruct.compile('u4u4')
 
-_decode_pressure_bs = bitstruct.compile('u4u4u16')
-_decode_temp_bs     = bitstruct.compile('u4u4u16')
-_decode_switches_bs = bitstruct.compile('u4u4u8u8')
-_decode_weight_bs   = bitstruct.compile('u4u4u24')
-_decode_ac_on_bs    = bitstruct.compile('u4u4u8')
+"""
+Send the bytestring message, msg over the _SERIAL_PORT at the _BAUDRATE. If expect_response is
+True, then the function hangs until the full response is recieved.
+"""
+def send(msg, expect_response = False):
+    # Clear serial port and write message
+    _ser.read()
+    _ser.write(msg)
+
+    # If response is expected, wait until recieved and then return
+    if expect_response:
+        while(_ser.in_waiting == 0):
+            pass
+        header = _header_decoder(_ser.read(1))
+        while(_ser.in_waiting != header[1]):
+            pass
+        return _ser.read(header[1])
+
+
+_MSG_ID_SET_LEDS      =  1
+_MSG_ID_SET_PUMP      =  2
+_MSG_ID_SET_SOLENOID  =  3
+_MSG_ID_SET_HEATER    =  4
+
 
 _set_heater_bs      = bitstruct.compile('u4u4u8')
 _set_pump_bs        = bitstruct.compile('u4u4u8')
@@ -40,89 +43,6 @@ _set_solenoid_bs    = bitstruct.compile('u4u4u8')
 _set_1gpio_bs        = bitstruct.compile('u4u4u1u7')
 _set_2gpio_bs        = bitstruct.compile('u4u4u1u7u1u7')
 _set_3gpio_bs        = bitstruct.compile('u4u4u1u7u1u7u1u7')
-
-class Reading:
-    def __init__(self) -> None:
-        self.timestamp = time()
-
-class PressureReading(Reading):
-    def __init__(self, value) -> None:
-        super().__init__()
-        self._raw_val = value
-    
-    def in_bar(self)->float:
-        return self._raw_val/1000.0 
-
-class TempuratureReading(Reading):
-    def __init__(self, value) -> None:
-        super().__init__()
-        self._raw_val = value
-    
-    def in_C(self)->float:
-        return self._raw_val/16.0
-    
-    def in_F(self)->float:
-        return self.in_C()*1.8+32
-
-class SwitchReading(Reading):
-    def __init__(self, pump_val, dial_val) -> None:
-        super().__init__()
-        self._pump_val = pump_val
-        self._dial_val = dial_val
-    
-    def pump(self)->bool:
-        return self._pump_val
-    
-    def dial(self)->int:
-        return self._dial_val
-
-class ScaleReading(Reading):
-    def __init__(self, raw_val) -> None:
-        super().__init__()
-        self._raw_val = raw_val
-    
-    def in_g(self)->float:
-        return -0.000152968191*self._raw_val+2491.937016352400
-
-class ACReading(Reading):
-    def __init__(self, val) -> None:
-        super().__init__()
-        self.val = val
-
-def get_pressure()->PressureReading:
-    ser.write(_get_pressure_msg)
-    while(ser.in_waiting != _MSG_LEN_GET_PRESSURE):
-        pass
-    response = _decode_pressure_bs.unpack(ser.read_all())
-    return PressureReading(response[2])
-
-def get_tempurature()->TempuratureReading:
-    ser.write(_get_temp_msg)
-    while(ser.in_waiting != _MSG_LEN_GET_TEMP):
-        pass
-    response = _decode_temp_bs.unpack(ser.read_all())
-    return TempuratureReading(response[2])
-
-def get_switches()->SwitchReading:
-    ser.write(_get_switch_msg)
-    while(ser.in_waiting != _MSG_LEN_GET_SWITCH):
-        pass
-    response = _decode_switches_bs.unpack(ser.read_all())
-    return SwitchReading(response[2], response[3])
-
-def get_weight():
-    ser.write(_get_weight_msg)
-    while(ser.in_waiting != _MSG_LEN_GET_WEIGHT):
-        pass
-    response = _decode_weight_bs.unpack(ser.read_all())
-    return ScaleReading(response[2])
-
-def get_ac_on() -> bool:
-    ser.write(_get_ac_on_msg)
-    while(ser.in_waiting != _MSG_LEN_GET_AC_ON):
-        pass
-    response = _decode_ac_on_bs.unpack(ser.read_all())
-    return ACReading(0!=response[2])
 
 def set_heater_to(new_value):
     ser.write(_set_heater_bs.pack(_MSG_ID_SET_HEATER, 1, new_value))
