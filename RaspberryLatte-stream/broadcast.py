6f97327d9bbadef5b2c782c6467199d77c50fc84
Richard Hall <hallboyone@icloud.com>
2022-06-13 22:25:58 -0400

    Test broadcast of brewlog

--- RaspberryLatte-stream/broadcast.py
+++ RaspberryLatte-stream/broadcast.py
@@ -4,10 +4,9 @@ from time import sleep
 UDP_IP = "192.168.0.126"
 UDP_PORT = 5005
 
-f = open("../brew_1.txt", 'r')
+f = open("2022-6-12-21:36:26.brewlog", 'r')
 lines = f.readlines()
 f.close()
-MESSAGE = "t=1655086881.5652995;temp=50.25;scale=-6.093334920294001;pressure=4.095;pump=0;heater=2.071128030594438;stage=0;"
 
 with socket.socket( socket.AF_INET, socket.SOCK_DGRAM ) as s:
     for ln in lines:
