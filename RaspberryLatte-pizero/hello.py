a23a3b6853650509c7007593766f702ac2e4f289
Richard Hall <hallboyone@icloud.com>
2022-06-15 09:46:45 -0400

    Removed old code

--- RaspberryLatte-pizero/hello.py
+++ RaspberryLatte-pizero/hello.py
@@ -3,10 +3,3 @@ from time import sleep
 
 gaggia_classic = EspressoMachine()
 gaggia_classic.run()
-
-# leds = LEDs()
-# while(True):
-#     leds.set_all(1,1,1)
-#     sleep(1)
-#     leds.set_all(0,0,0)
-#     sleep(1)
