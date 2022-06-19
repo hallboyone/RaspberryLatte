f495a86f3ceab967352b2f7a3c10e3f3893d3a40
Richard Hall <hallboyone@icloud.com>
2022-06-19 16:21:22 -0400

    Made file name abs in flash script

--- RaspberryLatte-pico/flash.sh
+++ RaspberryLatte-pico/flash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program ../build/RaspberryLatte-pico/RaspberryLattePico.elf verify reset exit"
+openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program /home/hallboyone/RaspberryLatte-pico/build/RaspberryLatte-pico/RaspberryLattePico.elf verify reset exit"
