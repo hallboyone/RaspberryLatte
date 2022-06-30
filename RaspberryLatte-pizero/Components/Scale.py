d05044a0b093054b6807cdb4ce396b620575c040
Richard Hall <hallboyone@icloud.com>
2022-06-30 17:21:39 -0400

    Updated scale with new uart bridge

--- RaspberryLatte-pizero/Components/Scale.py
+++ RaspberryLatte-pizero/Components/Scale.py
@@ -2,8 +2,9 @@ import bitstruct
 
 from PID import PIDSensor
 import uart_bridge
+import status_ids
 
-class Scale(uart_bridge.Getter, PIDSensor): 
+class Scale(uart_bridge.UARTMessenger, PIDSensor): 
     """
     Retreives the scale state from the espresso machine interfacing with the pico 
     firmware.
@@ -17,21 +18,25 @@ class Scale(uart_bridge.Getter, PIDSensor):
     to the last zeroed amount. The weight is returned in the units requested (either 'g' or
     'oz')
     """
-    _WEIGHT_REQUEST = bitstruct.pack('u4u4', uart_bridge.MSG_ID_GET_WEIGHT, 0)
-    _WEIGHT_DECODER = bitstruct.compile('u24')
 
     def __init__(self, min_dwell_time : float = 0.1) -> None:
-        super().__init__(min_dwell_time, self._WEIGHT_REQUEST, self._WEIGHT_DECODER)
+        uart_bridge.UARTMessenger.__init__(self, 0.1, False)
+        self.request_msg = bitstruct.pack('u4u4', uart_bridge.MSG_ID_GET_WEIGHT, 0)
+        self._decoder = bitstruct.compile('u24')
         self._origin = 0
 
     def zero(self):
-        uart_bridge.Getter.read(self)
-        self._origin = self._last_reading.val[0]
+        uart_bridge.UARTMessenger.send(self, self.request_msg)
+        self._origin = self._decoder.unpack(self.response)[0]
 
     def read(self, unit = 'g') -> float:
-        uart_bridge.Getter.read(self)
-        val_g = -0.000152968191*(self._last_reading.val[0] - self._origin)
-        if unit == 'g':
-            return val_g
-        elif unit == 'oz':
-            return 0.0352739619 * val_g
+        uart_bridge.UARTMessenger.send(self, self.request_msg)
+        if self.status != status_ids.SUCCESS:
+            print(f"Something went wrong with the scale's UART bridge: {self.status}")
+            return 0
+        else:
+            val_g = -0.000152710615479*(self._decoder.unpack(self.response)[0] - self._origin)
+            if unit == 'g':
+                return val_g
+            else:
+                return 0.0352739619 * val_g
