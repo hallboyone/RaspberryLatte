0400a9b656fc3a98d73dfe4ccfeaa841a27f14d9
Richard Hall <hallboyone@icloud.com>
2022-07-01 10:18:55 -0400

    Adding a Getter child class of UARTMessenger

--- RaspberryLatte-pizero/Components/ACSensor.py
+++ RaspberryLatte-pizero/Components/ACSensor.py
@@ -3,22 +3,22 @@ import bitstruct
 import uart_bridge
 import status_ids
 
-class ACSensor(uart_bridge.UARTMessenger): 
+class ACSensor(uart_bridge.Getter): 
     """
     Remote sensor object whose read() method returns True if AC power attached to 
     zerocross circut is hot.
     """
     def __init__(self, min_dwell_time : float = 0.025):
-        uart_bridge.UARTMessenger.__init__(self, min_dwell_time)
-        self.request_msg = bitstruct.pack('u4u4', uart_bridge.MSG_ID_GET_AC_ON, 0)
-        self._decoder = bitstruct.compile('u8')
+        uart_bridge.Getter.__init__(self, min_dwell_time, False,
+                                    request_message = bitstruct.pack('u4u4', uart_bridge.MSG_ID_GET_AC_ON, 0),
+                                    response_decoder = bitstruct.compile('u8'))
 
     def read(self)->bool:
-        uart_bridge.UARTMessenger.send(self, self.request_msg)
-        if self.status != status_ids.SUCCESS:
+        if uart_bridge.Getter.read(self) != status_ids.SUCCESS:
             print(f"Something went wrong with the ACStatus's UART bridge: {self.status}")
-            return False
-        return self._decoder.unpack(self.response)[0]!=0
+            return 0
+        else:
+            return (self.unpacked_response[0] != 0)
 
     def on(self)->bool:
         return self.read()
