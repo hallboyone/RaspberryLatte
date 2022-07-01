92592fbdda591ad182a12ccdc2b497d4f017ee3e
Richard Hall <hallboyone@icloud.com>
2022-07-01 10:02:08 -0400

    Updated temp sensor to new uart bridge

--- RaspberryLatte-pizero/Components/TempSensor.py
+++ RaspberryLatte-pizero/Components/TempSensor.py
@@ -2,17 +2,23 @@ import bitstruct
 
 from PID import PIDSensor
 import uart_bridge
+import status_ids
 
-class TempSensor(uart_bridge.Getter, PIDSensor): 
+class TempSensor(uart_bridge.UARTMessenger, PIDSensor): 
     _TEMP_REQUEST = bitstruct.pack('u4u4', uart_bridge.MSG_ID_GET_TEMP, 0)
     _TEMP_DECODER = bitstruct.compile('u16')
 
-    def __init__(self, min_dwell_time : float = 0.1) -> None:
-        super().__init__(min_dwell_time, self._TEMP_REQUEST, self._TEMP_DECODER)
+    def __init__(self) -> None:
+        uart_bridge.UARTMessenger.__init__(self, 0.1, False)
+        self.request_msg = bitstruct.pack('u4u4', uart_bridge.MSG_ID_GET_TEMP, 0)
+        self._decoder = bitstruct.compile('u16')
 
     def read(self, unit = 'C') -> float:
-        uart_bridge.Getter.read(self)
-        if unit == 'C':
-            return 0.0625 * self._last_reading.val[0]
+        uart_bridge.UARTMessenger.send(self, self.request_msg)
+        if self.status != status_ids.SUCCESS:
+            print(f"Something went wrong with the thermostat's UART bridge: {self.status}")
+            return 0
+        elif unit == 'C':
+            return 0.0625 * self._decoder.unpack(self.response)[0]
         else:
-            return 0.1125 * self._last_reading.val[0] + 32
+            return 0.1125 * self._decoder.unpack(self.response)[0] + 32
