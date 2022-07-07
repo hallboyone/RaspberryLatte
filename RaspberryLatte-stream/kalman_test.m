4c8f9087fb4cec80d35fef9380f29cecdcbf74c0
Richard Hall <rah75@duke.edu>
2022-07-07 10:21:10 -0400

    Updated plot_brew

--- RaspberryLatte-stream/kalman_test.m
+++ RaspberryLatte-stream/kalman_test.m
@@ -8,12 +8,15 @@ m(1) = 0;
 Ts = 0.05;
 
 F = [1 Ts; 0 1];
-Q = [Ts^2 Ts; Ts 1] * 5;
+Q = 0.05*[Ts^2 Ts; Ts 1];
+%Q = [0.05 1; 1 0.5];
 H = [1, 0];
-R = 1.5;
+R = 0.05;
 
-
+a = 0.075;
+v = nan(1, 50);
 x_filt = zeros(2, numel(m)+1);
+f_filt = zeros(1, numel(m)+1);
 P = 0.05*eye(2);
 for i=1:numel(m)
     x_predict = F*x_filt(:, i);
@@ -22,6 +25,8 @@ for i=1:numel(m)
     measurement = m(i);
     kalman_gain = P_predict*(H')/(H*P_predict*(H')+R);
     x_filt(:,i+1) = x_predict + kalman_gain*(measurement-H*x_predict);
+    
+    f_filt(:,i+1) = (1-a)*f_filt(:,i)+a*x_filt(2,i+1);
     P = (eye(2)-kalman_gain*H)*P*(eye(2)-kalman_gain*H)'+kalman_gain*R*(kalman_gain');
 end
 figure
@@ -29,5 +34,17 @@ subplot(2,1,1)
 plot([t', t'], [m', x_filt(1,1:end-1)'])
 legend(["Unfiltered Scale Readings", "Filtered Scale Readings"]);
 subplot(2,1,2)
-plot(t,x_filt(2,1:end-1))
-legend("Filtered Flowrate");
+plot([t',t'],[x_filt(2,1:end-1)',f_filt(1:end-1)'])
+
+legend(["Filtered Flowrate", "Smoothed Flowrate"]);
+
+figure
+plot(t',x_filt(2,1:end-1)')
+hold on
+for a = 0.005:0.005:0.1
+    f_filt = zeros(1, numel(m)+1);
+    for i=1:numel(m)
+        f_filt(:,i+1) = (1-a)*f_filt(:,i)+a*x_filt(2,i+1);
+    end
+    plot(t',f_filt(1:end-1)')
+end
--- RaspberryLatte-stream/plot_brew.m
+++ RaspberryLatte-stream/plot_brew.m
@@ -7,20 +7,24 @@ ran = false;
 %f.Position = [1224 1 691 1001];
 f.Position = [881 1 560 821];
 tlim = 10;
+colors = ['k', 'r', 'b', 'g'];
+brew_idx = 0;
 while true
     raw_dat = udpr();
     read_time = posixtime(datetime('now'));
     while isempty(raw_dat)
         raw_dat = udpr();
         if ran && posixtime(datetime('now')) - read_time > 5
-            f = figure;
+            %f = figure;
             dat_buf = [];
             start_time = 0;
             %f.Position = [1224 1 691 1001];
-            f.Position = [881 1 560 821];
-            tlim = 10;
+            %f.Position = [881 1 560 821];
+            %tlim = 10;
             ran = false;
+            brew_idx = brew_idx + 1;
         end
+        pause(0.1)
     end
     ran = true;
     dat = ParseLine(convertCharsToStrings(char(raw_dat)));
@@ -31,7 +35,7 @@ while true
     dat_buf = [dat_buf, dat];
 
     if numel(dat_buf)==5
-        tlim = plotdat(dat_buf, tlim);
+        tlim = plotdat(dat_buf, tlim, colors(mod(brew_idx,4)+1));
         dat_buf = [dat_buf(end)];
     end
 end
@@ -47,7 +51,7 @@ for i = 1:numel(vars)
 end
 end
 
-function tlim = plotdat(dat_buf, tlim)
+function tlim = plotdat(dat_buf, tlim, c)
 limits = struct('temp', [0, 100], 'scale', [0, 50], 'pressure', [0,15],...
     'pump', [0,150], 'heater', [0, 65], 'stage', [0,5]);
 if dat_buf(end).t > tlim
@@ -57,7 +61,7 @@ fn = fieldnames(dat_buf(1));
 for k=2:numel(fn)
     subplot(numel(fn)-1,1,k-1)
     hold on
-    plot([dat_buf(:).t], [dat_buf(:).(fn{k})], 'k')
+    plot([dat_buf(:).t], [dat_buf(:).(fn{k})], c)
     title(fn{k})
     xlim([0,tlim])
     ylim(limits.(fn{k}))
