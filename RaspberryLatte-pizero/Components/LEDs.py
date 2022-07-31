08301e58edd9947e354557724b775fa3cd5148c7
Richard Hall <hallboyone@icloud.com>
2022-07-31 14:02:01 -0400

    Updated the python LED class to use new binary_ouput sdk.

--- RaspberryLatte-pizero/Components/LEDs.py
+++ RaspberryLatte-pizero/Components/LEDs.py
@@ -10,23 +10,19 @@ class LEDs(uart_bridge.UARTMessenger):
     """
     def __init__(self) -> None:
         uart_bridge.UARTMessenger.__init__(self, 0, True)
-        self.msg_buf = bytearray(2)
-        bitstruct.pack_into('u4u4', self.msg_buf, 0, uart_bridge.MSG_ID_SET_LEDS, 1)
-        self.set_all(0,0,0)
+        self.msg_buf = bytearray(4)
+        bitstruct.pack_into('u4u4u8', self.msg_buf, 0, uart_bridge.MSG_ID_SET_LEDS, 3, 0)
+        self.set_all([0,0,0])
 
-    def send(self):
-        bitstruct.pack_into('u8', self.msg_buf, 8, self.last_setting)
-        uart_bridge.UARTMessenger.send(self, self.msg_buf)
-        if self.status != status_ids.SUCCESS:
-            print(f"Something went wrong with the LED's UART bridge: {self.status}")
-
-    def set_all(self, led0_val: bool, led1_val: bool, led2_val: bool):
-        self.last_setting = led2_val<<2 | led1_val<<1 | led0_val<<0
-        self.send()
+    def set_all(self, led_vals : list[bool]):
+        for i in range(3):
+            self.set(i, led_vals[i])
 
     def set(self, led_idx : int, state : bool):
         """Set the LED indicated by led_idx to the provided state"""
         if led_idx > 2 or led_idx < 0:
             raise RuntimeError("LED index must be in {0,1,2}")
-        self.last_setting = self.last_setting & ~(1<<led_idx) | state<<led_idx
-        self.send()
+        bitstruct.pack_into('u8u8', self.msg_buf, 16, led_idx, state)
+        uart_bridge.UARTMessenger.send(self, self.msg_buf)
+        if self.status != status_ids.SUCCESS:
+            print(f"Something went wrong with the LED's UART bridge: {self.status}")
