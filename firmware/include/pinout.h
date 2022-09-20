4fdce781e95853a73470c417610e2c019128ed2a
rah75 <rah75@duke.edu>
2022-09-20 19:57:08 -0400

    Updated LED pins to match V0.2 board

--- firmware/include/pinout.h
+++ firmware/include/pinout.h
@@ -11,8 +11,8 @@
 #define PUMP_SWITCH_PIN            9
 #define PHASECONTROL_OUT_PIN      10
 #define HEATER_PWM_PIN            11
-#define LED0_PIN                  13
-#define LED1_PIN                  12
+#define LED0_PIN                  12
+#define LED1_PIN                  13
 #define LED2_PIN                  15
 #define PRESSURE_SENSOR_PIN       28
 
