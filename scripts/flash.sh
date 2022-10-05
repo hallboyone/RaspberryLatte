a52537d4ab4d673c5cd544db8da0a06bdef22c2a
rah75 <rah75@duke.edu>
2022-10-04 21:26:39 -0400

    Deleted a few old scripts.

--- scripts/flash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python /home/hallboyone/RaspberryLatte-pico/scripts/reset_pico.py
-sleep 0.5s
-openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "program /home/hallboyone/RaspberryLatte-pico/build/pico_firmware/RaspberryLattePico.elf verify reset exit"
