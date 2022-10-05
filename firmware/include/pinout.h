9378864de7047576fcb8ae2cd6e3d423e73430c0
rah75 <rah75@duke.edu>
2022-10-04 21:05:54 -0400

    Updated pinout to reflect new board

--- firmware/include/pinout.h
+++ firmware/include/pinout.h
@@ -3,7 +3,6 @@
 
 #define I2C_SDA_PIN                2
 #define I2C_SCL_PIN                3
-#define PHASECONTROL_0CROSS_PIN    4
 #define SOLENOID_PIN               5
 #define LMT01_DATA_PIN             6
 #define DIAL_A_PIN                 7
@@ -11,9 +10,10 @@
 #define PUMP_SWITCH_PIN            9
 #define PHASECONTROL_OUT_PIN      10
 #define HEATER_PWM_PIN            11
-#define LED0_PIN                  12
+#define LED2_PIN                  12
 #define LED1_PIN                  13
-#define LED2_PIN                  15
+#define PHASECONTROL_0CROSS_PIN   14
+#define LED0_PIN                  15
 #define PRESSURE_SENSOR_PIN       28
 
 
