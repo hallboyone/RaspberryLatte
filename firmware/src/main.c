04b66ab47613222ed4d41841f143184bb9b0d57a
Richard Hall <rah75@duke.edu>
2022-12-20 17:59:45 -0500

    Added flowmeter testing code

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -2,17 +2,21 @@
 
 #include "espresso_machine.h"
 
-#include "machine_settings.h"
+#include "flow_meter.h"
 #include "pinout.h"
 
 int main(){
     // Setup UART
     stdio_uart_init_full(PICO_DEFAULT_UART_INSTANCE, 115200, PICO_DEFAULT_UART_TX_PIN, PICO_DEFAULT_UART_RX_PIN);
 
-    espresso_machine_viewer espresso_machine;
-    if(espresso_machine_setup(&espresso_machine)){
-        return 1;
-    }
+
+    // espresso_machine_viewer espresso_machine;
+    // if(espresso_machine_setup(&espresso_machine)){
+    //     return 1;
+    // }
+
+    flow_meter fm;
+    flow_meter_setup(&fm, FLOW_RATE_PIN, 1.0);
 
     // Run main machine loop
     absolute_time_t next_msg_time = get_absolute_time();
@@ -22,13 +26,14 @@ int main(){
     
     while(true){
         next_loop_time = make_timeout_time_us(loop_period_us);
-        espresso_machine_tick();
+        //espresso_machine_tick();
         
         if(absolute_time_diff_us(get_absolute_time(), next_msg_time) < 0){
             next_msg_time = make_timeout_time_us(msg_period_us);
-            if(espresso_machine->boiler.setpoint){
-                printf("T = %0.2f / %0.2f\n", espresso_machine->boiler.temperature/16., espresso_machine->boiler.setpoint/16.);
-            }
+            // if(espresso_machine->boiler.setpoint){
+            //     printf("T = %0.2f / %0.2f\n", espresso_machine->boiler.temperature/16., espresso_machine->boiler.setpoint/16.);
+            // }
+            printf("%0.4f x %0.4f\n", flow_meter_volume(&fm), flow_meter_rate(&fm));
         }
 
         sleep_until(next_loop_time);
