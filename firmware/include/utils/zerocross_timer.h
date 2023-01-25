597ae6fed771f384f7201d4396cffcd98fdb8fe0
Richard Hall <rah75@duke.edu>
2023-01-24 23:09:06 -0500

    Adding zerocross_timer lib
    This will pull the zerocross logic out of phasecontrol. This will separate the AC power status from the pump logic.

---
+++ firmware/include/utils/zerocross_timer.h
@@ -0,0 +1,21 @@
+/** \defgroup zerocross_timer Zerocross Timer Library
+ * \ingroup utils
+ * \brief Tracks the lates zerocrossing time. Uses this to determine if an AC circuit is hot.
+ * 
+ * A zerocross circuit toggles a GPIO whenever an AC sin wave crosses zero. By tracking when
+ * this happens, it is possible to deduce if the sin wave is active (implying AC power).
+ * 
+ * @{
+ * 
+ * \file zerocross_timer.h
+ * \author Richard Hall (hallboyone@icloud.com)
+ * \brief Zerocross Timer header
+ * \version 0.1
+ * \date 2023-01-24
+ */
+#ifndef ZEROCROSS_TIMER_H
+#define ZEROCROSS_TIMER_H
+
+
+#endif
+/** @} */
