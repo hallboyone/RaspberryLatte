e9d4f271c38d95fd8ed986c12dd4b43d3414d2a3
hallboyone <hallboyone@icloud.com>
2023-02-10 09:14:22 -0500

    Improved picoprobe script and include path

--- scripts/open_serial_picoprobe_pi.sh
+++ scripts/open_serial_picoprobe_pi.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+while getopts ':l' OPTION; do
+    case "$OPTION" in
+        l)
+            log_path="~/raspberrylatte_logs/$(date +"%j.%H.%M.%S").log"
+            log_path="~/raspberrylatte_logs/log"
+            mkdir -p ~/raspberrylatte_logs
+            cmd=(sudo minicom -D /dev/ttyACM0 -b 115200 -C "$log_path")
+            echo "${cmd[@]}"
+            ${cmd[@]}
+            echo "Log saved at: $log_path"
+            sleep 1
+            exit 1
+            ;;
+        ?)
+            echo "Invalid option. Use -l to enable logging"
+            exit 1
+            ;;
+    esac
+done
 sudo minicom -D /dev/ttyACM0 -b 115200
