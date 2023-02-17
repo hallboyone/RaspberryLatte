835bdf1ce716a5aa50dd1c05c0ed0c0f27e5f04a
Richard Hall <rah75@duke.edu>
2023-02-16 23:22:49 -0500

    Update ulka_pump.c
    Improved pressure comp.

--- firmware/src/drivers/ulka_pump.c
+++ firmware/src/drivers/ulka_pump.c
@@ -63,27 +63,17 @@ float ulka_pump_get_flow(ulka_pump * p){
 
 float ulka_pump_get_pressure(ulka_pump * p){
     float pressure = 0;
-    if(p->percent_power == 0) return 0;
-    else if(p->percent_power < 10){
-        pressure = 0.4319*p->percent_power - 0.6476*ulka_pump_get_flow(p);
-    } else if(p->percent_power < 20){
-        pressure = 2.6426 + 0.0686*p->percent_power - 1.0042*ulka_pump_get_flow(p);
-    } else if(p->percent_power < 30){
-        pressure = 4.0434 + 0.0640*p->percent_power - 1.2913*ulka_pump_get_flow(p);
-    } else if(p->percent_power < 40){
-        pressure = 2.5994 + 0.1425*p->percent_power - 1.5014*ulka_pump_get_flow(p);
-    } else if(p->percent_power < 50){
-        pressure = 2.3161 + 0.1532*p->percent_power - 1.5692*ulka_pump_get_flow(p);
-    } else if(p->percent_power < 60){
-        pressure = 1.8617 + 0.1626*p->percent_power - 1.6878*ulka_pump_get_flow(p);
-    } else if(p->percent_power < 70){
-        pressure = 5.6301 + 0.0955*p->percent_power - 1.6701*ulka_pump_get_flow(p);
-    } else if(p->percent_power < 80){
-        pressure = 6.5122 + 0.0847*p->percent_power - 1.6412*ulka_pump_get_flow(p);
-    } else if(p->percent_power < 90){
-        pressure = 2.4047 + 0.1405*p->percent_power - 1.6984*ulka_pump_get_flow(p);
+    if(p->percent_power < 5){
+        // Low zone without offset. Not very accurate
+        pressure = 0.4318*p->percent_power - 0.6473*ulka_pump_get_flow(p);
+    } else if(p->percent_power<10){
+        // Between low and high zone. Linear interp between vals
+        const float p1 = 0.4318*p->percent_power - 0.6473*ulka_pump_get_flow(p);
+        const float p2 = 3.3201 + 0.1256*p->percent_power - 1.5993*ulka_pump_get_flow(p);
+        pressure = p1 + (p2-p1)*(p->percent_power-5)/5.0;
     } else {
-        pressure = 3.5282 + 0.1258*p->percent_power - 1.6838*ulka_pump_get_flow(p);
+        // High zone. More accurate
+        pressure = 3.3201 + 0.1256*p->percent_power - 1.5993*ulka_pump_get_flow(p);
     }
     return (pressure > 0 ? pressure : 0);
 }
