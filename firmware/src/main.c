8b789ddec1a95776cf6efa5c441c052194bf02f1
hallboyone <hallboyone@icloud.com>
2023-03-17 09:08:00 -0400

    Cleaned up main.

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,8 +1,10 @@
+//#define PRINT_MACHINE_STATUS_OVER_UART
+
+#ifdef PRINT_MACHINE_STATUS_OVER_UART
 #include <stdio.h>
+#endif
 
 #include "machine_logic/espresso_machine.h"
-#include "machine_logic/machine_settings.h"
-#include "pinout.h"
 
 #define S_TO_US 1000000
 #define MS_TO_US 1000
@@ -12,28 +14,26 @@ int main(){
     stdio_uart_init_full(PICO_DEFAULT_UART_INSTANCE, 115200, PICO_DEFAULT_UART_TX_PIN, PICO_DEFAULT_UART_RX_PIN);
 
     espresso_machine_viewer espresso_machine;
-    if(espresso_machine_setup(&espresso_machine)){
-        return 1;
-    }
-    machine_settings * settings = machine_settings_acquire();
-
-    // Message rate limiter
-    absolute_time_t next_msg_time = get_absolute_time();
-    const uint64_t msg_period_us = 200 * MS_TO_US;
+    if(espresso_machine_setup(&espresso_machine)) return 1;
 
     // Loop rate limiter
     absolute_time_t next_loop_time;
     const uint64_t loop_period_us = 10 * MS_TO_US;
 
+    #ifdef PRINT_MACHINE_STATUS_OVER_UART
+    // Message rate limiter
+    absolute_time_t next_msg_time = get_absolute_time();
+    const uint64_t msg_period_us = 200 * MS_TO_US;
+    #endif
+    
     while(true){
         next_loop_time = make_timeout_time_us(loop_period_us);
 
         // Update the machine
         espresso_machine_tick();
-        if(espresso_machine->pump.flowrate_ml_s < 0){
-            printf("Warning\n");
-        }
-        // Print status once a second
+
+        #ifdef PRINT_MACHINE_STATUS_OVER_UART
+        // Print status periodically 
         if(absolute_time_diff_us(get_absolute_time(), next_msg_time) < 0){
             next_msg_time = make_timeout_time_us(msg_period_us);
             if(espresso_machine->switches.ac_switch){
@@ -47,6 +47,8 @@ int main(){
                 espresso_machine->pump.pressure_bar);
             }
         }
+        #endif
+
         sleep_until(next_loop_time);
     }
 }
