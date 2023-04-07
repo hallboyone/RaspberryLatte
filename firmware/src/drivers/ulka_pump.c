ce2d22dcc6b72ed6e7c40a7c54759cf651907182
hallboyone <hallboyone@icloud.com>
2023-04-06 22:09:47 -0400

    Refactored ulka_pump. Hacked in 9.5bar pressure cap for testing

--- firmware/src/drivers/ulka_pump.c
+++ firmware/src/drivers/ulka_pump.c
@@ -38,6 +38,34 @@ static const uint8_t _percent_to_power_lut [] = {
     105, 106, 107, 107, 108, 109, 109, 110, 111, 111, 112, 113, 113, 114, 115, 115, 116, 
     117, 118, 118, 119, 120, 120, 121, 122, 122, 123, 124, 124, 125, 126, 126, 127};
 
+#define NUM_LINEAR_REGIONS 10 // should be divisor of 100 and match linear coefficients arrays below
+static const uint8_t LINEAR_REGION_SPAN = 100/NUM_LINEAR_REGIONS;
+static const float OFFSET [NUM_LINEAR_REGIONS]    = { 0,      2.6426, 4.0434, 2.5994, 2.3161, 1.8617, 5.6301, 6.5122, 2.4047, 3.5282};
+static const float PUMP_GAIN [NUM_LINEAR_REGIONS] = { 0.4319, 0.0686, 0.0640, 0.1425, 0.1532, 0.1626, 0.0955, 0.0847, 0.1405, 0.1258};
+static const float FLOW_GAIN [NUM_LINEAR_REGIONS] = {-0.6476,-1.0042,-1.2913,-1.5014,-1.5692,-1.6878,-1.6701,-1.6412,-1.6984,-1.6838};
+
+/**
+ * \brief Returns the pump power needed to maintain the target pressure.
+ * \param p The ulka_pump that should be regulated.
+ * \param target_pressure The pressure that is being targeted
+ * \returns The pump power required to reach the target pressure, clipped between 0 and 100. 
+*/
+static uint8_t ulka_pump_power_to_get_pressure(ulka_pump p, const float target_pressure){
+    if(p->flow_ml_s == NULL || target_pressure < 0) return 0;
+    
+    const float flowrate = ulka_pump_get_flow_ml_s(p);
+    for(uint i = 0; i < NUM_LINEAR_REGIONS; i++){
+        // Check if the power in each linear region is strong enough to reach flowrate. If it is, then compute
+        // the required power and return.
+        if(OFFSET[i] + PUMP_GAIN[i]*LINEAR_REGION_SPAN*(i+1) + FLOW_GAIN[i]*flowrate > target_pressure){
+            float power = (target_pressure - FLOW_GAIN[i]*flowrate - OFFSET[i])/PUMP_GAIN[i];
+            return (power < 0 ? 0 : (uint8_t)power);
+        }
+    }
+    // None of the regions are strong enough. Return full strength.
+    return 100;
+}
+
 ulka_pump ulka_pump_setup(uint8_t zerocross_pin, uint8_t out_pin, int32_t zerocross_shift_us, uint8_t zerocross_event){
     ulka_pump p = malloc(sizeof(ulka_pump_));
     p->locked = true;
@@ -56,7 +84,8 @@ int ulka_pump_setup_flow_meter(ulka_pump p, uint8_t pin_num, float ml_per_tick){
 
 void ulka_pump_pwr_percent(ulka_pump p, uint8_t power_percent){
     if(!p->locked){
-        p->power_percent = (power_percent > 100 ? 100 : power_percent);
+        const uint8_t max_power = ulka_pump_power_to_get_pressure(p, 9.5);
+        p->power_percent = (power_percent > max_power ? max_power : power_percent);
         phasecontrol_set_duty_cycle(&(p->driver), _percent_to_power_lut[p->power_percent]);
     }
 }
@@ -84,29 +113,12 @@ float ulka_pump_get_flow_ml_s(ulka_pump p){
 
 float ulka_pump_get_pressure_bar(ulka_pump p){
     if(p->flow_ml_s == NULL) return 0;
-    float pressure = 0;
-    if(p->power_percent == 0) return 0;
-    else if(p->power_percent < 10){
-        pressure =  0.4319*p->power_percent - 0.6476*ulka_pump_get_flow_ml_s(p);
-    } else if(p->power_percent < 20){
-        pressure = 2.6426 +  0.0686*p->power_percent - 1.0042*ulka_pump_get_flow_ml_s(p);
-    } else if(p->power_percent < 30){
-        pressure = 4.0434 +  0.0640*p->power_percent - 1.2913*ulka_pump_get_flow_ml_s(p);
-    } else if(p->power_percent < 40){
-        pressure = 2.5994 + 0.1425*p->power_percent - 1.5014*ulka_pump_get_flow_ml_s(p);
-    } else if(p->power_percent < 50){
-        pressure = 2.3161 + 0.1532*p->power_percent - 1.5692*ulka_pump_get_flow_ml_s(p);
-    } else if(p->power_percent < 60){
-        pressure = 1.8617 + 0.1626*p->power_percent - 1.6878*ulka_pump_get_flow_ml_s(p);
-    } else if(p->power_percent < 70){
-        pressure = 5.6301 +  0.0955*p->power_percent - 1.6701*ulka_pump_get_flow_ml_s(p);
-    } else if(p->power_percent < 80){
-        pressure = 6.5122 +  0.0847*p->power_percent - 1.6412*ulka_pump_get_flow_ml_s(p);
-    } else if(p->power_percent < 90){
-        pressure = 2.4047 + 0.1405*p->power_percent - 1.6984*ulka_pump_get_flow_ml_s(p);
-    } else {
-        pressure = 3.5282 + 0.1258*p->power_percent - 1.6838*ulka_pump_get_flow_ml_s(p);
-    }
+
+    // Index of the linear region indexed by the percent power
+    const uint8_t active_region = p->power_percent/LINEAR_REGION_SPAN;
+    const float pressure =   OFFSET[active_region] 
+                     + PUMP_GAIN[active_region]*p->power_percent 
+                     + FLOW_GAIN[active_region]*ulka_pump_get_flow_ml_s(p);
     return (pressure > 0 ? pressure : 0);
 }
 
